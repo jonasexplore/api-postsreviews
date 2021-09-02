047af7d4940865cd9a8ec4f1111747dd6eb22c71
jonasexplore <jonasexplore@gmail.com>
2021-09-02 16:17:45 -0300

    feat: create schema

--- src/index.js
+++ src/index.js
@@ -1,83 +1,10 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
+const schema = require("./schema");
+const prisma = new PrismaClient({ log: ["query"] });
 
-const typeDefs = gql`
-  scalar DateTime
-  type User {
-    id: Int
-    nome: String
-    email: String
-    createdAt: DateTime
-    posts: [Post]
-  }
-
-  type Post {
-    id: Int
-    titulo: String
-    conteudo: String
-  }
-
-  type Query {
-    users: [User]
-    postsByUser(id: Int): [Post]
-    postsByReviewer(id: Int): [Post]
-  }
-
-  type Mutation {
-    createUserAndPost(
-      nome: String
-      email: String
-      titulo: String
-      conteudo: String
-    ): User
-  }
-`;
-
-const resolvers = {
-  Query: {
-    users: async () =>
-      await prisma.user.findMany({
-        include: {
-          posts: true,
-        },
-      }),
-    postsByUser: async (_, args) =>
-      await prisma.user
-        .findUnique({
-          where: { id: Number(args.id) },
-        })
-        .posts(),
-    postsByReviewer: async (_, args) =>
-      await prisma.review
-        .findUnique({
-          where: { id: Number(args.id) },
-        })
-        .reviewer()
-        .posts(),
-  },
-  Mutation: {
-    createUserAndPost: async (_, args) => {
-      const { nome, email, titulo, conteudo } = args;
-
-      return await prisma.user.create({
-        data: {
-          nome,
-          email,
-          posts: {
-            create: {
-              titulo,
-              conteudo,
-            },
-          },
-        },
-      });
-    },
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers, context: prisma });
+const server = new ApolloServer({ schema, context: { prisma } });
 server.listen({ port: 4000 }, () =>
   console.log(`Servidor pronto em localhost:4000`)
 );
---
+++ src/schema.js
@@ -0,0 +1,57 @@
+const { objectType, queryType, makeSchema } = require("@nexus/schema");
+const { nexusPrisma } = require("nexus-plugin-prisma");
+const path = require("path");
+
+const User = objectType({
+  name: "User",
+  definition(t) {
+    t.model.id();
+    t.model.nome();
+    t.model.email();
+    t.model.createdAt();
+    t.model.posts();
+  },
+});
+
+const Post = objectType({
+  name: "Post",
+  definition(t) {
+    t.model.id();
+    t.model.titulo();
+    t.model.conteudo();
+    t.model.publicado();
+    t.model.autor();
+    t.model.createdAt();
+  },
+});
+
+const Review = objectType({
+  name: "Review",
+  definition(t) {
+    t.model.id();
+    t.model.post();
+    t.model.reviewer();
+    t.model.nota();
+    t.model.createdAt();
+  },
+});
+
+const Query = queryType({
+  definition(t) {
+    t.list.field("users", {
+      type: "User",
+      resolve: (_, __, { prisma }) => prisma.user.findMany(),
+    });
+  },
+});
+
+const schema = makeSchema({
+  types: [Query, Review, Post, User],
+  plugins: [nexusPrisma()],
+  outputs: {
+    schema: path.join(__dirname, "schema.graphql"),
+    typegen: path.join(__dirname, "../prisma/generated", "nexus.ts"),
+  },
+});
+
+module.exports = schema;
