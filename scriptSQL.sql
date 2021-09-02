a15e39e1cdfe5dd3983566f5ccdb7a52f33e5ca8
jonasexplore <jonasexplore@gmail.com>
2021-09-02 14:53:00 -0300

    feat: initial commit

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -1,28 +1,32 @@
+CREATE SEQUENCE users_seq;
+
 CREATE TABLE users(
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id INT DEFAULT NEXTVAL ('users_seq') PRIMARY KEY,
    nome VARCHAR(255) NOT NULL,
    email VARCHAR(255) NOT NULL,
-   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+   createdAt TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE SEQUENCE posts_seq;
+
 CREATE TABLE posts(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	titulo VARCHAR(255) NOT NULL,
+   id INT DEFAULT NEXTVAL ('posts_seq') PRIMARY KEY,
+   titulo VARCHAR(255) NOT NULL,
    conteudo TEXT NOT NULL,
    autorId INT NOT NULL,
    publicado BOOLEAN,
-	createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY(autorId) REFERENCES users(id)
+   createdAt TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY(autorId) REFERENCES users(id)
 );
 
 INSERT INTO users (nome, email)
 VALUES 
-   ("Amanda Costa", "a@a.com"),
-   ("Beatriz Martins", "b@b.com"),
-   ("Carla Souza", "c@c.com");
+   ('Amanda Costa', 'a@a.com'),
+   ('Beatriz Martins', 'b@b.com'),
+   ('Carla Souza', 'c@c.com');
 
 INSERT INTO posts (titulo, conteudo, autorId, publicado)
 VALUES 
-   ("Iniciando em JS", "Conteúdo para iniciantes", 1 , true),
-   ("Dicas de React", "Melhores práticas", 2 , true),
-   ("Sintaxe CommonJS", "Usando módulos",3 , false);
+   ('Iniciando em JS', 'Conteúdo para iniciantes', 1 , true),
+   ('Dicas de React', 'Melhores práticas', 2 , true),
+   ('Sintaxe CommonJS', 'Usando módulos', 3 , false);
